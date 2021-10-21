1ef787c7100a3dceb81650596ea0926f221954b1
ruslanways <ruslanways@gmail.com>
2021-10-21 17:17:06 +0300

    added hw1, hw3

---
+++ ruslanways/HW_1/hw1_2.py
@@ -0,0 +1,11 @@
+print("\n")
+
+a = int(input("Enter 'a': "))
+b = int(input("Enter 'b': "))
+print("a+b =", a+b)
+print("a-b =", a-b)
+print("a*b =", a*b)
+print("a/b =", a/b)
+print("a**b =", a**b)
+
+print("\n")
---
+++ ruslanways/HW_3/hw3_3.py
@@ -0,0 +1,6 @@
+a = 23
+b = 17
+b = a
+a = b
+print(a)
+print(b)
