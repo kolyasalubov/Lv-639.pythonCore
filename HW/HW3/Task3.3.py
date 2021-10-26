de5f0913e48f04c6ef4fc7a2ddb7001c64d789e1
Dmytro Khomitskyi <khom.dmytro.w@gmail.com>
2021-10-27 01:07:17 +0300

    add new folder

---
+++ HW/HW3/Task3.3.py
@@ -0,0 +1,7 @@
+n = int(input("Enter the number 1:"))
+num_str1=str(n)
+j=int(input("Enter the number 2:"))
+num_str2=str(j)
+print(num_str1,num_str2)
+num_str1,num_str2=num_str2,num_str1
+print(num_str1,num_str2)
