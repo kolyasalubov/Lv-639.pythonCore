56c20662d341db7d496bb8b21a037757fa07e242
SKseniia <skseniias@gmail.com>
2021-10-26 21:11:13 +0300

    HW commit

---
+++ SadovaHW/HW1/homework1.2.py
@@ -0,0 +1,8 @@
+number_1=int(input("Enter a = "))
+number_2=int(input("Enter b = "))
+print(f"a+b={number_1 + number_2}")
+print(f"a-b={number_1 - number_2}")
+print(f"a*b={number_1 * number_2}")
+print(f"a/b={number_1 / number_2}")
+print(f"a**b={number_1 ** number_2}")
+
