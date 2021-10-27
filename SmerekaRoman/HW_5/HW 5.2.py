b7f8fd38207d31004a62a05a66ce5897b3261ce3
smerekaroman <super-romansmereka@ukr.net>
2021-10-27 19:41:45 +0300

    fixed 5.2

--- SmerekaRoman/HW_5/HW 5.2.py
+++ SmerekaRoman/HW_5/HW 5.2.py
@@ -1,6 +1,6 @@
 login_user = input("Please input your login: ")
 while login_user != "First": 
     print("Wrong user name")
-    login_user = input("Please input another logn: ")
+    login_user = input("Please input another login: ")
 else:
     print("Hello First!")
