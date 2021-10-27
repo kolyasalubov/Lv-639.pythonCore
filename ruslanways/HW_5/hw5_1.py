9432c058ddb6f542985ee9a84533a3b6f0ad849c
ruslanways <ruslanways@gmail.com>
2021-10-27 22:52:23 +0300

    added homework_5

---
+++ ruslanways/HW_5/hw5_1.py
@@ -0,0 +1,5 @@
+even_divisible_by_2 = [k for k in range(1,11) if k%2==0]
+print(even_divisible_by_2)
+
+odd_divisible_by_3 = [k for k in range(1,11) if k%2!=0 and k%3==0]
+print(odd_divisible_by_3)
---
+++ ruslanways/HW_5/hw5_2.py
@@ -0,0 +1,7 @@
+while True:
+    login_user = input("Please enter your login: ")
+    if login_user == "First":
+        print("Good day to you, First!")
+        break
+    else:
+        print("Error! Please enter right login!")
