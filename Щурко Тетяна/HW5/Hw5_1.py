ac4049bc2ecd29eb7e2ad2ffaf5d13fe8ce162ee
TATA <49610189+TaniaShchurko@users.noreply.github.com>
2021-10-28 17:11:34 +0300

    Update Hw5_1.py

--- Щурко Тетяна/HW5/Hw5_1.py
+++ Щурко Тетяна/HW5/Hw5_1.py
@@ -1,6 +1,6 @@
 even_numbers = [i for i in range(11) if i % 2 == 0]
 odd_numbers = [i for i in range(11) if i % 3 == 0]
-numbers_not_divide_two_tree = [i for i in range(11) if i % 2 == 1 and i % 3 == 1]
+numbers_not_divide_two_tree = [i for i in range(11) if i % 2 == 1 or i % 3 == 1]
 print(f"All even numbers in range [0,10] are {even_numbers}")
 print(f"All odd numbers in range [0,10] are {odd_numbers}")
 print(f"All numbers that don`t divide to 2 and 3 in range [0,10] are {numbers_not_divide_two_tree}")
