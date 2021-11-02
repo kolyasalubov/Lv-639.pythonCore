c0503cd54b7e91a3c7dc6df76f8a413e267fd0f9
Dmytro Khomitskyi <khom.dmytro.w@gmail.com>
2021-11-02 18:45:07 +0200

    change

--- HW/HW5/HW 5.2.py
+++ HW/HW5/HW 5.2.py
@@ -1,2 +1,8 @@
-log=input("Write log name -")
-if log=="First"
+while 1:
+    log=input("Write log name - ")
+    if log=="First":
+        print("Greet the user")
+        break
+    else:
+        print ("Error")
+
