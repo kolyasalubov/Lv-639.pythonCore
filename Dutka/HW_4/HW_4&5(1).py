3ca495ffdffb594af82bf19dec13def14234d440
0Andreychuk0 <real.andreychuk@icloud.com>
2021-11-05 17:12:41 +0200

    added HW 4.1, 4.2

---
+++ Dutka/HW_4/HW_4&5(1).py
@@ -0,0 +1,28 @@
+print("")
+print("▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬")
+print("   PLEASE LIKE, PYTHON CORE    ")
+print("▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬")
+print("                   Dutka©")
+print("")
+
+
+print("Визначу фекторіал: ")
+a = int(input("Твоя цифра: "))
+
+b = 1
+while a > 1:
+    b = b * a
+    a = a - 1
+
+print(b)
+
+####################################
+
+# print("Визначу фекторіал: ")
+# a = int(input("Твоя цифра: "))
+
+# b = 1
+# for i in range(2, a+1):
+#     b = b * i
+
+# print(b)
---
+++ Dutka/HW_4/HW_4&5(2).py
@@ -0,0 +1,10 @@
+print("")
+print("▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬")
+print("   PLEASE LIKE, PYTHON CORE    ")
+print("▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬")
+print("                   Dutka©")
+print("")
+
+n = int(input("Цифра: "))
+for val in range(n):
+    print(float(val))
