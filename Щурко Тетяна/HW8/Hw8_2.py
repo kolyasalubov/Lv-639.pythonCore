e8bd75fb6683ec730ef1591161290861a426692b
TATA <49610189+TaniaShchurko@users.noreply.github.com>
2021-11-10 10:56:42 +0200

    Update Hw8_2.py

--- Щурко Тетяна/HW8/Hw8_2.py
+++ Щурко Тетяна/HW8/Hw8_2.py
@@ -1,13 +1,19 @@
 class Human:
+    species = "Homosapiens"
+
     def __init__(self, name):
         self.name = name
-        self.species = "Homosapiens"
+
     def welcome_message(self):
         print(f"Welcome {self.name}!!!")
-    def return_species(self):
-        return self.species
-    def arbitraty_message(self):
-        message=input(f"Enter your message to {self.name} = ")
+
+    @classmethod
+    def return_species(cls):
+        return cls.species
+
+    @staticmethod
+    def arbitraty_message():
+        message=input(f"Enter your message = ")
         print(f"Message is {message}")
 
 Maria = Human("Maria")
