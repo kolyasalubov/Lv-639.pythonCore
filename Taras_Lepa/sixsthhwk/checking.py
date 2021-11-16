1f4c5bb4178914c47f22d2c4e2776cf851bcbf43
Taras Lepa <taras.lepa@gmail.com>
2021-11-16 18:13:06 +0200

    added six hwk

---
+++ Taras_Lepa/sixsthhwk/checking.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+
+def checking_simb(st):
+    param = list(st)
+    param = Counter(param)
+    return param
+
+
+
---
+++ Taras_Lepa/sixsthhwk/func_31.py
@@ -0,0 +1,7 @@
+def run(first,second):
+    if first>second:
+        return first
+    elif(first==second):
+        return("They =")
+    else:
+        return second
